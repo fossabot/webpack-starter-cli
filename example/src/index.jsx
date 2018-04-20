ed39888b745adf938ba631f3b05a267e68310c1e
Arno Schutijzer <arno.schutijzer@gmail.com>
2018-04-20 18:57:29 +0200

    chore(docs): add an example

---
+++ example/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+
+ReactDOM.render(
+  <div id="title">Hello world!</div>,
+  document.getElementById('app')
+);
